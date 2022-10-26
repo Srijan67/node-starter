72a83ae0cb97e5fdd67f6aac5da9ab970bb2d7ad
Srijan Sinha <srijan@scizers.in>
2022-10-26 16:20:46 +0530

    swagger

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
-const errorMiddleware = require("./middleware/error");
-app.use(express.json());
+const dotenv = require("dotenv");
 
+const errorMiddleware = require("./middleware/error");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+app.use(express.json());
+dotenv.config({ path: "backend/config/config.env" });
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API Project EComm",
+      version: "1.0.0",
+      description: "this is test",
+    },
+    servers: [{ url: `http://localhost:${process.env.PORT}` }],
+  },
+  apis: ["backend/routes/*.js"],
+};
+const SwaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(SwaggerSpec));
 //route imports
 const products = require("./routes/productRoutes");
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,16 @@ const {
 } = require("../controller/productController");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/products:
+ *  get:
+ *    summary: To get all products
+ *    description: this api is use to fetch data from mongodb
+ *    responses:
+ *      200:
+ *        description: success
+ */
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 router
